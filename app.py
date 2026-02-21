d6f2eb66338fc7805c663622ef68912f3ab13e68
IgorsavinovN <ikix46@gmail.com>
2026-02-21 14:46:26 +0100

    Initial commit: Flask website for DevOps learning

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import datetime
+import os
+import calendar
+app = Flask(__name__)
+
+# Домашняя страница
+@app.route('/')
+def home():
+    return render_template('index.html', 
+                          message="Привет, DevOps-инженер!",
+                          time=datetime.datetime.now())
+
+# Страница "О нас"
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Страница с информацией о сервере
+@app.route('/system')
+def system_info():
+    info = {
+        'hostname': os.uname().nodename,
+        'os': os.uname().sysname,
+        'python_version': os.sys.version,
+        'current_dir': os.getcwd(),
+        'user': os.environ.get('USER', 'unknown')
+    }
+    return render_template('system.html', info=info)
+
+# Календарь
+@app.route('/calendar.html')
+def calendar_page():
+    # Получаем текущий год и месяц
+    now = datetime.datetime.now()
+    year = now.year
+    month = now.month
+
+    # Генерируем календарь месяца в виде HTML
+    cal = calendar.HTMLCalendar(firstweekday=0)  # Понедельник первый день
+    month_calendar = cal.formatmonth(year, month)
+
+    return render_template('calendar.html', month_calendar=month_calendar, year=year, month=month)
+
+
+
+# Запуск приложения
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
